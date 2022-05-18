7a7e018ea21522f451d0b626a5249fed3c59f5e3
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-05-17 20:35:14 -0400

    Week 2 Completed Code

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const http = require('http');
+const connect = require('connect');
+const app = connect();
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": 'text/plain'
-    });
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+}
+
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-}).listen(3000);
+}
 
-console.log('Server running at http://localhost:3000');
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World');
+}
+
+app.use(logger);
+
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
+
+// //Our first custom middleware
+// app.use(helloWorld);
+
+app.listen(3000);
+
+console.log('Server is running at http://localhost:3000/');
